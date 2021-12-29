c13a303939a091ebace4faeb6e15e1da8d653730
teguh dana prayuda <megtrotech@gmail.com>
2021-12-29 16:59:31 +0700

    initial commit

---
+++ src/components/Content.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <div className="w-full h-full p-10">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
+            repudiandae a totam excepturi laborum placeat atque voluptatibus
+            consequuntur cumque quis maiores doloribus, quibusdam accusamus,
+            velit fugiat dignissimos nostrum officia veritatis, iure perferendis
+            nihil porro expedita consequatur nesciunt! Modi, cupiditate neque
+            cum quo facilis consequuntur magni fuga sunt facere eaque, iusto
+            deleniti delectus architecto aliquam provident quisquam
+            necessitatibus voluptates est ab quod libero veniam! Asperiores ea
+            eaque exercitationem quos? Architecto sunt alias ullam aspernatur
+            minima hic in deserunt deleniti voluptatem ad sit corrupti est at
+            amet mollitia nam, neque velit. Mollitia at sed quo, animi labore,
+            beatae provident recusandae amet in deleniti asperiores? Modi eaque
+            velit ab architecto rem placeat aperiam, ea laborum quis praesentium
+            nulla recusandae, quia quod ipsa esse voluptatibus ullam. Asperiores
+            inventore laboriosam, nihil debitis saepe minus sint, optio
+            repudiandae culpa, accusantium quas cupiditate corrupti maiores
+            voluptatem aut vel modi deserunt placeat eligendi tenetur numquam
+            quasi. Repellat inventore numquam laborum non omnis amet nulla,
+            similique iusto consequuntur facilis cumque, rem neque doloribus.
+            Illo fuga culpa totam sit iusto, repudiandae delectus incidunt animi
+            unde quod quis, harum possimus at nihil? Distinctio quae officia
+            labore sequi iure eligendi earum dolorum voluptates illo a est, eius
+            adipisci rem. Neque, expedita optio? Lorem ipsum, dolor sit amet
+            consectetur adipisicing elit. Culpa repudiandae a totam excepturi
+            laborum placeat atque voluptatibus consequuntur cumque quis maiores
+            doloribus, quibusdam accusamus, velit fugiat dignissimos nostrum
+            officia veritatis, iure perferendis nihil porro expedita consequatur
+            nesciunt! Modi, cupiditate neque cum quo facilis consequuntur magni
+            fuga sunt facere eaque, iusto deleniti delectus architecto aliquam
+            provident quisquam necessitatibus voluptates est ab quod libero
+            veniam! Asperiores ea eaque exercitationem quos? Architecto sunt
+            alias ullam aspernatur minima hic in deserunt deleniti voluptatem ad
+            sit corrupti est at amet mollitia nam, neque velit. Mollitia at sed
+            quo, animi labore, beatae provident recusandae amet in deleniti
+            asperiores? Modi eaque velit ab architecto rem placeat aperiam, ea
+            laborum quis praesentium nulla recusandae, quia quod ipsa esse
+            voluptatibus ullam. Asperiores inventore laboriosam, nihil debitis
+            saepe minus sint, optio repudiandae culpa, accusantium quas
+            cupiditate corrupti maiores voluptatem aut vel modi deserunt placeat
+            eligendi tenetur numquam quasi. Repellat inventore numquam laborum
+            non omnis amet nulla, similique iusto consequuntur facilis cumque,
+            rem neque doloribus. Illo fuga culpa totam sit iusto, repudiandae
+            delectus incidunt animi unde quod quis, harum possimus at nihil?
+            Distinctio quae officia labore sequi iure eligendi earum dolorum
+            voluptates illo a est, eius adipisci rem. Neque, expedita optio?
+        </div>
+    );
+};
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="text-center bg-gray-700">
+            © 2021-2022 Gusti Riza Pradana
+        </footer>
+    );
+};
+
+export default Footer;
